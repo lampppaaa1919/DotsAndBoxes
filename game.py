5bea06317087d19c84d0099d96dfe638cc7af190
lampppaaa1919 <filiptashkov1@gmail.com>
2026-02-08 16:31:14 +0100

    Successors, search
    Refactor DotsAndBoxes class and add game logic

--- game.py
+++ game.py
@@ -1,7 +1,10 @@
-from searching_framework import Problem
+from random import random
+
+from searching_framework import Problem, astar_search
+
 
 class DotsAndBoxes(Problem):
-    def __init__(self, initial, goal, conquered_lines):
+    def __init__(self, initial, conquered_lines):
         super().__init__(initial,goal)
         self.conquered_lines = conquered_lines
 
@@ -25,8 +28,20 @@ class DotsAndBoxes(Problem):
                         ((i1,j1),(i1-1,j1)) in self.conquered_lines and \
                         ((i2,j2),(i2-1,j2)) in self.conquered_lines:
                     pts+=1
-            
-                    
+        elif j1==j2:
+            if j1<3:
+                if((i1,j1),(i1+1,j1)) in self.conquered_lines and \
+                        ((i1,j1),(i1,j1+1)) in self.conquered_lines and \
+                        ((i1+1,j1),(i2+1,j2) in self.conquered_lines):
+                    pts+=1
+            elif j1>0:
+                if((i1,j1),(i1-1,j1)) in self.conquered_lines and \
+                        ((i2,j2),(i2-1,j2)) in self.conquered_lines and \
+                        ((i1-1,j1),(i2-1,j2)) in self.conquered_lines:
+                    pts+=1
+        return pts
+
+
 
     def actions(self, state):
         return self.successor(state).keys()
@@ -36,26 +51,42 @@ class DotsAndBoxes(Problem):
 
     def successor(self, state):
         succs = dict()
-        
+        # ptsPlr 0 ptsOpp 0 turn agent-a
+        plr, opp, turn = state
+        #kje se bira od funkciite vo genetic
+        new_line = index(int(random()))[-1]
+        conquered_lines = self.addLine(new_line)
+        if turn == "agent-a":
+            self.conquerSquares(new_line, plr)
+            succs["Igrachot ja nacrta linijata "+index]=(plr, opp, "agent-b")
+        elif turn == "agent-b":
+            self.conquerSquares(new_line, opp)
+            succs["Protivnikot ja nacrta linijata "+index]=(plr, opp, "agent-a")
         return succs
 
     def goal_test(self, goal):
         return self.goal == goal
 
-
+def getAllLines(n,m):
+    allLines = list()
+    totalLines=0
+    for i in range(n):
+        for j in range(m):
+            if i+1<4:
+                allLines.append((totalLines,(i,j),(i+1,j)))
+                totalLines+=1
+            if j+1<4:
+                allLines.append((totalLines,(i,j),(i,j+1)))
+                totalLines+=1
+    return tuple(allLines)+(allLines,)
 
 if __name__ == "__main__":
     goal=0
     conquered_lines = ()
-    gridLines = list()
-    for i in range(4):
-        for j in range(4):
-            if i+1<4:
-                gridLines.append((goal,(i,j),(i+1,j)))
-                goal+=1
-            if j+1<4:
-                gridLines.append((goal,(i,j),(i,j+1)))
-                goal+=1
-    game=DotsAndBoxes((0,0,"agent-a"),goal,conquered_lines)
+    gridLines = getAllLines(4,4)
+    game=DotsAndBoxes((0,0,"agent-a"),conquered_lines)
+    playgame=astar_search(game)
+    print(playgame.solve())
+    print(playgame.solution())
     # for line in gridLines:
     #     print(line)
