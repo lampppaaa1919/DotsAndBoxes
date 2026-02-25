c8eba761919b8d4f50933392481a57dddf2f04d9
231164 <filip.tashkov@students.finki.ukim.com>
2026-02-25 12:16:15 +0100

    Updated expectimax commands

--- searching_framework/expectimax.py
+++ searching_framework/expectimax.py
@@ -1,5 +1,7 @@
 class Node:
     def __init__(self, value, children=None):
+        if children is None:
+            children = []
         self.value = value
         self.children = children
         # self.left = None
@@ -7,6 +9,9 @@ class Node:
 
 
 # Initializing Nodes to None
+def root(v):
+    return Node(v)
+
 def new_node(v, parent):
     temp = Node(v)
     parent.children.append(temp)
@@ -16,7 +21,7 @@ def new_node(v, parent):
 # Getting expectimax
 def expectimax(node, is_max):
     # Condition for Terminal node
-    if node.children is None:
+    if len(node.children) == 0:
         return node.value
 
     # Maximizer node. Chooses the max from the
@@ -32,10 +37,10 @@ def expectimax(node, is_max):
     # the left and right subtrees
     else:
         # return (expectimax(node.left, True) + expectimax(node.right, True)) / 2
-        sum = 0
+        sum_nodes = 0
         for child in node.children:
-            sum += expectimax(child, True)
-        return sum / len(node.children)
+            sum_nodes += expectimax(child, True)
+        return sum_nodes / len(node.children)
 
 
 # Driver code
@@ -48,15 +53,29 @@ if __name__ == '__main__':
     # But because the entire tree
     # is searched their
     # values don't matter
-    root = new_node(0)
-    root.left = new_node(0)
-    root.right = new_node(0)
+    root = root(0)
+    child1 = new_node(0,root)
+    for v in [10,20,30]:
+        new_node(v, child1)
+    child2 = new_node(0,root)
+    for v in [5,10,15]:
+        new_node(v, child2)
+    child3 = new_node(0,root)
+    for v in [20,20,25]:
+        new_node(v, child3)
+    child4 = new_node(0,root)
+    for v in [40,48]:
+        new_node(v, child4)
+
+    res = expectimax(root,True)
+    # root.left = new_node(0)
+    # root.right = new_node(0)
 
     # Assigning values to Leaf nodes
-    root.left.left = new_node(10)
-    root.left.right = new_node(10)
-    root.right.left = new_node(9)
-    root.right.right = new_node(100)
+    # root.left.left = new_node(10)
+    # root.left.right = new_node(10)
+    # root.right.left = new_node(9)
+    # root.right.right = new_node(100)
 
-    res = expectimax(root, True)
+    # res = expectimax(root, True)
     print("Expectimax value is " + str(res))
